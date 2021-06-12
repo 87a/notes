8ce84e0e2f5c7454b7de469f10264228f0ef1f57
87a <13738325103@126.com>
2021-06-12 19:15:34 +0800

    6/12

--- test.m
+++ test.m
@@ -1,4 +1,26 @@
-A=eye(5);
-t=[0:0.1:1];
-y=sin(t);
-plot(t,y);
+% A=eye(5);
+% t=[0:0.1:1];
+% y=sin(t);
+% figure(1);
+% plot(t,y);
+% disp(A);
+% figure(2);
+% imagesc(A),colorbar,colormap gray;
+A=zeros(10,1);
+% for i = 1:10
+%     A(i)=2^i;
+%     if i==6
+%         break;
+%     end;
+% end;
+% i=1;
+% while true
+%     A(i)=999;
+%     i=i+1;
+%     if i==6
+%         break;
+%     end;
+% end;
+% disp(A);
+y=square(5);
+disp(y);
